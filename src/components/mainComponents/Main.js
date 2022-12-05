5877c68dc61fa2295acc41fee31723beaabfa867
regina <komi.regina@mail.ru>
2022-12-05 18:09:26 +0300

    why

--- src/components/mainComponents/Main.js
+++ src/components/mainComponents/Main.js
@@ -1,8 +1,11 @@
+
+import TextSection from "../secComponents/TextSection"
 import "./Main.css"
 
 function Main() {
     return (
         <main className="main">
+            <TextSection></TextSection>
             
         </main>
     )
---
+++ src/components/secComponents/TextSection.js
@@ -0,0 +1,9 @@
+import "./TextSection.css";
+
+function TextSection(){
+    return(
+        <div classname="text-section">О, этот изумительный макет прекрасен</div>
+    )
+}
+
+export default TextSection
